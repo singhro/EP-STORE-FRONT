5dad37d203a5c09679cc369bcc5f157fa0d68d21
devkant.m <devkant.m@hcl.com>
2019-06-06 18:42:23 +0530

    added HTTPS protocol

--- src/redux/action.js
+++ src/redux/action.js
@@ -5,7 +5,7 @@ import Axios from 'axios';
 export const fetchAccesstoken=()=>(dispatch)=>{
         Axios({
             method: 'post',
-            url: 'http://ffz-sandbox-2.epdemos.com/cortex/oauth2/tokens?scope=wawastore&role=PUBLIC&grant_type=password',
+            url: 'https://ffz-sandbox-2.epdemos.com/cortex/oauth2/tokens?scope=wawastore&role=PUBLIC&grant_type=password',
             data: {
             },
             headers:{'Content-Type': 'application/x-www-form-urlencoded'}
@@ -20,7 +20,7 @@ export const getProducts=(accessToken,skuid)=>(dispatch)=>{
     console.log("check token",accessToken)
     Axios({
         method: 'post',
-        url: 'http://ffz-sandbox-2.epdemos.com/cortex/items/wawastore/lookups/form?zoom=definition:components:element:components:element:standaloneitem:definition,definition:components:element:components:element:standaloneitem:price,definition:components:element:standaloneitem:recommendations:accessory:element:definition,definition:components:element:standaloneitem:price,definition:components:element:standaloneitem:definition,definition:components:element:standaloneitem:recommendations:accessory:element:price&followlocation',
+        url: 'https://ffz-sandbox-2.epdemos.com/cortex/items/wawastore/lookups/form?zoom=definition:components:element:components:element:standaloneitem:definition,definition:components:element:components:element:standaloneitem:price,definition:components:element:standaloneitem:recommendations:accessory:element:definition,definition:components:element:standaloneitem:price,definition:components:element:standaloneitem:definition,definition:components:element:standaloneitem:recommendations:accessory:element:price&followlocation',
         data: {
             "code" : skuid
         },
@@ -37,7 +37,7 @@ export const getProductsHoagies=(accessToken,skuid)=>(dispatch)=>{
     console.log("check token",accessToken)
     Axios({
         method: 'post',
-        url: 'http://ffz-sandbox-2.epdemos.com/cortex/items/wawastore/lookups/form?zoom=definition,definition:components:element:components:element:standaloneitem:definition,definition:components:element:standaloneitem:code,definition:components:element:standaloneitem:price,definition:components:element:components:element:standaloneitem:price,definition:components:element:components:element:components:element:standaloneitem:definition,definition:components:element:components:element:components:element:standaloneitem:price,recommendations:accessory:element:definition,recommendations:accessory:element:price,definition:components:element&followlocation',
+        url: 'https://ffz-sandbox-2.epdemos.com/cortex/items/wawastore/lookups/form?zoom=definition,definition:components:element:components:element:standaloneitem:definition,definition:components:element:standaloneitem:code,definition:components:element:standaloneitem:price,definition:components:element:components:element:standaloneitem:price,definition:components:element:components:element:components:element:standaloneitem:definition,definition:components:element:components:element:components:element:standaloneitem:price,recommendations:accessory:element:definition,recommendations:accessory:element:price,definition:components:element&followlocation',
         data: {
             "code" : skuid
         },
